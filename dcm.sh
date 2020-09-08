f4365827b8a98d5e5d6bd22a0c107a299882839a
Yusra23 <javeed.studies@gmail.com>
2020-09-08 20:27:07 +0530

    health

---
+++ dcm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+echo CPU Load: `top -bn1 | grep load | awk '{print $10 " " $11 " " $12}'`
+#top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+
+
+# free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }'
+# df -h | awk '$NF=="/"{printf "%s\t\t", $5}'
+# top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}'
