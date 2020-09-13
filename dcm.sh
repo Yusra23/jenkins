6015b03bc8e319c5c5991aacf60af9eac6d78e19
Yusra23 <48834585+Yusra23@users.noreply.github.com>
2020-09-13 11:28:31 +0530

    Update dcm.sh

--- dcm.sh
+++ dcm.sh
@@ -3,7 +3,6 @@ free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 
 df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
 echo CPU Load: `top -bn1 | grep load | awk '{print $10 " " $11 " " $12}'`
 echo CPUs: `top -b -n1 | grep "Cpu" | awk '{print $2 " " $3 " " $8 " " $9}'`
-echo "test"
 echo "ngrok"
 
 
